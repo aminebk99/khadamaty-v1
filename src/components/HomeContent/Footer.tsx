d9049f63824acd1c7b71711cead5769c1a6c2516
Mohamed Amine Bazaou <129026688+aminebk99@users.noreply.github.com>
2025-02-05 20:10:58 +0100

    commit

--- src/components/HomeContent/Footer.tsx
+++ src/components/HomeContent/Footer.tsx
@@ -1,38 +1,79 @@
-import { Link } from "react-router-dom"
+import React from "react";
 
-function Footer() {
+const Footer = () => {
   return (
-    <footer className="grid grid-cols-4 bg-green-800 text-white justify-center py-6">
-        <div className="flex flex-col items-center">
-            <h3 className="text-xl">Liens utiles</h3>
-            <Link to={""}>A propos</Link>
-            <Link to={""}>FAQ</Link>
-            <Link to={""}>Blog</Link>
-            <Link to={""}>Nous Contactez</Link>
-            <Link to={""}>Securité</Link>
-        </div>
-        <div className="flex flex-col items-center">
-        <h3 className="text-xl">Opportunités</h3>
-            <Link to={""}>Emploi</Link>
-            <Link to={""}>Khadamatt pour les partenaires</Link>
-            <Link to={""}>Khadamaty Business</Link>
-        </div>
-        <div className="flex flex-col items-center">
-        <h3 className="text-xl">Nous Suivre</h3>
-            <Link to={""}>Facebook</Link>
-            <Link to={""}>Twitter</Link>
-            <Link to={""}>Instagram</Link>
-        </div>
-        <div className="flex flex-col items-center">
-            <Link to={""}>Conditions d'utilisation</Link>
-            <Link to={""}>Politique de confidentialité</Link>
-            <Link to={""}>Politique en matiere de cookie</Link>
-            <Link to={""}>Conformité</Link>
+    <footer className="relative bg-[#0F5748] text-white">
+        <div className="custom-shape-divider-top-1738780874">
+    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path d="M600,112.77C268.63,112.77,0,65.52,0,7.23V120H1200V7.23C1200,65.52,931.37,112.77,600,112.77Z" className="shape-fill"></path>
+    </svg>
+</div>
+      {/* Curved Shape */}
+      <div className="relative">
+        {/* <div className="absolute w-full h-24 bg-[#D2A341] top-0 rounded-es-4xl"></div> */}
+        {/* <div className="absolute w-full h-20 bg-[#0F5748] top-6 rounded-es-4xl"></div> */}
+      </div>
+
+      {/* Footer Content */}
+      <div className="relative z-10 max-w-6xl mx-auto px-6 py-12">
+        <div className="grid md:grid-cols-3 gap-8 text-center md:text-left">
+          {/* Useful Links */}
+          <div>
+            <h4 className="text-lg font-semibold mb-3">Liens utiles</h4>
+            <ul className="space-y-2">
+              <li><a href="#" className="hover:underline">À propos</a></li>
+              <li><a href="#" className="hover:underline">FAQ</a></li>
+              <li><a href="#" className="hover:underline">Blog</a></li>
+              <li><a href="#" className="hover:underline">Nous contacter</a></li>
+              <li><a href="#" className="hover:underline">Sécurité</a></li>
+            </ul>
+          </div>
+
+          {/* Opportunities */}
+          <div>
+            <h4 className="text-lg font-semibold mb-3">Opportunités</h4>
+            <ul className="space-y-2">
+              <li><a href="#" className="hover:underline">Emploi</a></li>
+              <li><a href="#" className="hover:underline">Khadamaty pour les partenaires</a></li>
+              <li><a href="#" className="hover:underline">Khadamaty Business</a></li>
+            </ul>
+          </div>
+
+          {/* Social Media */}
+          <div>
+            <h4 className="text-lg font-semibold mb-3">Nous suivre</h4>
+            <ul className="space-y-2">
+              <li><a href="#" className="hover:underline">Facebook</a></li>
+              <li><a href="#" className="hover:underline">Twitter</a></li>
+              <li><a href="#" className="hover:underline">Instagram</a></li>
+            </ul>
+          </div>
         </div>
 
-      
+        {/* Download Buttons */}
+        <div className="flex justify-center md:justify-start space-x-4 mt-8">
+          <img src="/google-play.png" alt="Google Play" className="h-10 cursor-pointer" />
+          <img src="/app-store.png" alt="App Store" className="h-10 cursor-pointer" />
+        </div>
+
+        {/* Policies */}
+        <div className="mt-6 text-sm text-gray-300 text-center md:text-left">
+          <a href="#" className="hover:underline">Conditions d'utilisation</a> ·
+          <a href="#" className="hover:underline"> Politique de confidentialité</a> ·
+          <a href="#" className="hover:underline"> Politique en matière de cookies</a> ·
+          <a href="#" className="hover:underline"> Conformité</a>
+        </div>
+
+        {/* Language Selector */}
+        <div className="mt-6 flex justify-center md:justify-start">
+          <select className="bg-white text-black px-3 py-2 rounded">
+            <option value="fr">Français</option>
+            <option value="en">English</option>
+          </select>
+        </div>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
