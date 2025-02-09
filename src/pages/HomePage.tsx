602ef11a0f11b3aa9db107cacc2ee0de0933359c
Mohamed Amine Bazaou <129026688+aminebk99@users.noreply.github.com>
2025-02-09 17:05:35 +0100

    commit logo

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,7 +2,7 @@ import Header from "../components/Header"
 import Categories from "../components/HomeContent/Categories"
 import Footer from "../components/HomeContent/Footer"
 import Hero from "../components/HomeContent/Hero"
-import MainContent from "../components/HomeContent/MainContent"
+
 import Service from "../components/HomeContent/Service"
 
 function HomePage() {
