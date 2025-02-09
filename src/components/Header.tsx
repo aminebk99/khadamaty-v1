bcb92e0f5203f0c63408c642b1d2af0a37803af6
Mohamed Amine Bazaou <129026688+aminebk99@users.noreply.github.com>
2025-02-09 16:58:16 +0100

    commit logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,15 @@
-
-
+import logo from "../assets/khadamatylogomcd.png"
 function Header() {
   return (
     <header className='bg-[#146551] w-full flex items-center justify-between px-10 py-5'>
         <div className='text-white'>
-            {/* <img src={logo} alt="" /> */}
-            logo
-
+            <img src={logo} alt="" className="w-40"/>
+            {/* logo */}
         </div>
         <div className=''>
             <button className='px-6 py-2 border border-white font-medium text-white mr-6 rounded-xl'>Connexion</button>
             <button className='bg-white px-6 py-2 rounded-xl font-medium text-[#146551]'>Inscription</button>
         </div>
-      
     </header>
   )
 }
--- src/components/HomeContent/Service.tsx
+++ src/components/HomeContent/Service.tsx
@@ -12,29 +12,35 @@ function Service() {
       <div className="w-full h-full flex flex-col justify-center items-center">
         <h1 className="text-4xl font-bold">Meilleurs services</h1>
         <div className="w-full flex justify-end my-10">
-          <span className="shadow-lg p-2 mr-2 rounded-full ">
-            <MoveLeft />
+          <span className="shadow-lg p-2 mr-2 rounded-full cursor-pointer">
+            <MoveLeft size={24} />
           </span>
-          <span className="shadow-lg p-2 rounded-full ">
-            <MoveRight />
+          <span className="shadow-lg p-2 rounded-full cursor-pointer">
+            <MoveRight size={24} />
           </span>
         </div>
-        <div className="p-4 rounded-2xl shadow w-100">
-          <div className="flex items-center">
-            <div className="h-10 w-10 rounded-full bg-black"></div>
-            <span className="ml-4">name of users</span>
-          </div>
-          <div className="w-full h-60 my-10 rounded-xl bg-blue-200"></div>
-          <div>
-            <h3 className="text-2xl font-bold">Dépannage Plombier</h3>
-            <p className="my-4">{truncatedText}</p>
-            <div className="w-full flex items-center justify-center ">
-              <button className="px-4 py-2 bg-[#146551] text-white font-medium border-none rounded-xl w-[80%]">
-                Contactez-moi
-              </button>
+        
+        <div className="w-full grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 bg-amber-300 p-4 rounded-xl">
+          {[1, 2, 3, 4].map((item) => (
+            <div key={item} className="p-4 rounded-2xl shadow w-full bg-white">
+              <div className="flex items-center">
+                <div className="h-10 w-10 rounded-full bg-black"></div>
+                <span className="ml-4 font-medium text-gray-700">Nom de l'utilisateur</span>
+              </div>
+              <div className="w-full h-60 my-10 rounded-xl bg-blue-200"></div>
+              <div>
+                <h3 className="text-2xl font-bold">Dépannage Plombier</h3>
+                <p className="my-4 text-gray-600">{truncatedText}</p>
+                <div className="w-full flex items-center justify-center">
+                  <button className="px-4 py-2 bg-[#146551] text-white font-medium border-none rounded-xl w-[80%]">
+                    Contactez-moi
+                  </button>
+                </div>
+              </div>
             </div>
-          </div>
+          ))}
         </div>
+
       </div>
     </div>
   );
