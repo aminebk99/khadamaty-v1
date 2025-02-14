19251c0a4613652b55055fa34cef44445db79210
Mohamed Amine Bazaou <129026688+aminebk99@users.noreply.github.com>
2025-02-14 19:16:29 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import ProfilePage from './pages/ProfilePage'
-import ReclamationPage from './pages/reclamation'
 
 function App() {
   return (
@@ -9,7 +8,6 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />}/>
         <Route path='/home' element={<HomePage />}/>
-        <Route path='/reclamation' element={<ReclamationPage />}/>
         <Route path='/profile' element={<ProfilePage />}/>
       </Routes>
     </BrowserRouter>
